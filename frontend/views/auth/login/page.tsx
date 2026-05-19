721668ca458984738ba9c8fda18040ff3ca83f80
Shah-Kalp-5871 <kalp.5871@gmail.com>
2026-05-19 17:21:30 +0530

    Merge receptionist login fix and push everything to main

--- frontend/views/auth/login/page.tsx
+++ frontend/views/auth/login/page.tsx
@@ -26,7 +26,7 @@ import { ROUTES, ROLE_REDIRECT_MAP } from '@/constants/routes';
 // Helper to set cookies
 const setCookie = (name: string, value: string, days: number) => {
   const expires = new Date(Date.now() + days * 864e5).toUTCString();
-  document.cookie = `${name}=${value}; expires=${expires}; path=/opd/; SameSite=Lax`;
+  document.cookie = `${name}=${value}; expires=${expires}; path=/; SameSite=Lax`;
 };
 
 const StaticLoginView = () => {
